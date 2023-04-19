b2e28e3c34c0d984a725b4c2e13bc5764a8702f1
Borja Lopez Diaz <borja.lpzdz5@gmail.com>
2023-04-19 19:12:44 +0200

    Instalamos de manera global nodemon, express y dotenv

--- 6.- Node/Dia 3/Express/server.js
+++ 6.- Node/Dia 3/Express/server.js
@@ -3,6 +3,14 @@ require("dotenv").config();
 
 const app = new express();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening to port ${process.env.PORT}`);
-})
+app.get("/greeting/:name/:age")
+
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server listening to port ${process.env.PORT}`);
+// })
+
+app.get("/hello", function (req, res) {
+  res.send("Hello world");
+});
+
+app.listen(8000);
